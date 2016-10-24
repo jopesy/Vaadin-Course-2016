e56ec9fe95703917980116ffa636e7a287eac259
Matias Paasi <Matias Paasi>
2016-10-24 11:48:16 +0300

    DB muokattu ja lisätty DatabaseHelper, josta toivottavasti on apua tiedon haun kanssa

--- auctions.sql
+++ auctions.sql
@@ -32,7 +32,7 @@ CREATE TABLE `bids` (
   KEY `userid` (`userid`),
   CONSTRAINT `bids_ibfk_1` FOREIGN KEY (`itemid`) REFERENCES `items` (`itemid`) ON DELETE CASCADE,
   CONSTRAINT `bids_ibfk_2` FOREIGN KEY (`userid`) REFERENCES `users` (`userid`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -41,6 +41,7 @@ CREATE TABLE `bids` (
 
 LOCK TABLES `bids` WRITE;
 /*!40000 ALTER TABLE `bids` DISABLE KEYS */;
+INSERT INTO `bids` VALUES (4,5,1,300.00),(5,7,3,421.50),(6,7,4,422.00),(7,7,3,430.00),(8,9,1,88.88);
 /*!40000 ALTER TABLE `bids` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -56,7 +57,6 @@ CREATE TABLE `items` (
   `brand` char(25) DEFAULT NULL,
   `model` char(25) DEFAULT NULL,
   `descr` varchar(255) DEFAULT NULL,
-  `photo` varchar(255) DEFAULT NULL,
   `userid` int(11) DEFAULT NULL,
   `buynow` decimal(10,2) DEFAULT NULL,
   `startprice` decimal(10,2) NOT NULL DEFAULT '0.00',
@@ -65,7 +65,7 @@ CREATE TABLE `items` (
   PRIMARY KEY (`itemid`),
   KEY `items_ibfk_1` (`userid`),
   CONSTRAINT `items_ibfk_1` FOREIGN KEY (`userid`) REFERENCES `users` (`userid`) ON DELETE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -74,7 +74,38 @@ CREATE TABLE `items` (
 
 LOCK TABLES `items` WRITE;
 /*!40000 ALTER TABLE `items` DISABLE KEYS */;
+INSERT INTO `items` VALUES (5,'Nopsa','500',NULL,3,NULL,299.99,'2016-11-30 00:00:00',1),(7,'Crescent','Salto','Vähän käytetty hybridipyörä',1,680.00,420.00,'2016-12-12 00:00:00',1),(8,'Tunturi',NULL,'Vanha ja hieman ruosteinen naistenpyörä',1,NULL,10.00,'2016-12-12 00:00:00',1),(9,'Helkama','Jopo','Melko uusi, hyvässä kunnossa oleva punainen Jopo.',4,300.00,50.00,'2016-12-24 00:00:00',1),(19,'Helkama','Jopo','Hieno Jopo.',5,NULL,20.25,'2016-12-20 00:00:00',1);
 /*!40000 ALTER TABLE `items` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `photos`
+--
+
+DROP TABLE IF EXISTS `photos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `photos` (
+  `photoid` int(11) NOT NULL AUTO_INCREMENT,
+  `itemid` int(11) DEFAULT NULL,
+  `phototype` varchar(45) DEFAULT NULL,
+  `photo` blob,
+  `photosize` varchar(45) DEFAULT NULL,
+  `photoname` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`photoid`),
+  KEY `itemid` (`itemid`),
+  CONSTRAINT `photos_ibfk_1` FOREIGN KEY (`itemid`) REFERENCES `items` (`itemid`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `photos`
+--
+
+LOCK TABLES `photos` WRITE;
+/*!40000 ALTER TABLE `photos` DISABLE KEYS */;
+INSERT INTO `photos` VALUES (2,9,'.jpeg',NULL,'200x200','Jopo'),(3,19,'.jpg',NULL,'100x100','hienojopo');
+/*!40000 ALTER TABLE `photos` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -91,8 +122,9 @@ CREATE TABLE `users` (
   `email` varchar(255) DEFAULT NULL,
   `phone` char(12) DEFAULT NULL,
   `password` varchar(255) NOT NULL,
+  `username` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`userid`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -101,7 +133,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (1,'Matias','Paasi','matias@gmail.com','0445878722','salasana123');
+INSERT INTO `users` VALUES (1,'Matti','Meikäläinen','mattimeik@gmail.com','0501231234','salasana123','mattimeik'),(3,'Mikko','Mallikas','mikko-mallikas@yahoo.com','044771122','salasana1','mikmal11'),(4,'Essi','Esimerkki','essi89@hotmail.com','040897867','qwerty123456','essi89'),(5,'Teppo','Testaaja','teppo@gmail.com','040123123','salasana12345','teppotestaaja');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -114,4 +146,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2016-10-13 12:51:22
+-- Dump completed on 2016-10-24 11:42:04
---
+++ bicycles/src/main/java/com/mycompany/bicycles/DatabaseHelper.java
@@ -0,0 +1,301 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.bicycles;
+
+//Importtaa koko javan sql libraryn
+import java.sql.*;
+
+public class DatabaseHelper {
+    
+    //Lisää käyttäjä
+    static void addUser(){
+    try{
+        Class.forName("com.mysql.jdbc.Driver");  
+        Connection con=DriverManager.getConnection(  
+        "jdbc:mysql://localhost:3306/auctions?zeroDateTimeBehavior=convertToNull", "root", "root");  
+        //Määrittelee yhteyden databaseen ja yhdistää
+        
+        //Muuttujia testausta varten, muuttujien arvot voi vaihtaa kentistä luettaviin arvoihin?
+        String firstname = "Teppo";
+        String lastname = "Testaaja";
+        String email = "teppo@gmail.com";
+        String phone = "040123123";
+        String password = "salasana12345";
+        String username = "teppotestaaja";
+
+        //SQL queryn määrittely
+        PreparedStatement stmt = con.prepareStatement("INSERT INTO users(firstname, lastname, email, phone, password, username) VALUES(?,?,?,?,?,?)");
+        //Jokaisen queryn ? merkin kohdalle määritellään arvo aiemmin määritettyjen muuttujien mukaan
+        stmt.setString(1,firstname);
+        stmt.setString(2,lastname);
+        stmt.setString(3,email);
+        stmt.setString(4,phone);
+        stmt.setString(5,password);
+        stmt.setString(6,username);
+        //Suorittaa queryn
+        stmt.executeUpdate();
+        //Sulkee yhteyden databaseen
+        con.close(); 
+    }catch(Exception e){ System.out.println(e);} //try ja catch pakollisia
+}
+
+    //Poista käyttäjä
+static void deleteUser(){
+    try{
+        Class.forName("com.mysql.jdbc.Driver");  
+        Connection con=DriverManager.getConnection(  
+        "jdbc:mysql://localhost:3306/auctions?zeroDateTimeBehavior=convertToNull", "root", "root");
+        //Yhteys
+        
+        int userid = 6; //Käytettävä muuttuja
+        PreparedStatement stmt = con.prepareStatement("DELETE FROM users WHERE userid=?"); //Query
+        stmt.setInt(1,userid); //WHERE lausekkeen arvon määritys
+        stmt.executeUpdate(); //Queryn suoritus
+        con.close(); //Yhteyden katkaisu
+    }catch(Exception e){ System.out.println(e);}
+}
+
+//Lisää esine ja sen kuva
+static void addItem(){
+    try{
+        Class.forName("com.mysql.jdbc.Driver");  
+        Connection con=DriverManager.getConnection(  
+        "jdbc:mysql://localhost:3306/auctions?zeroDateTimeBehavior=convertToNull", "root", "root");
+        
+        String brand = "Helkama";
+        String model = "Jopo";
+        String descr = "Hieno Jopo.";
+        String userid = "5";
+        String buynow = null;
+        String startprice = "20.25";
+        String enddate = "2016-12-20";
+        String photoid = null;
+        
+        PreparedStatement stmt = con.prepareStatement("INSERT INTO items(brand, model, descr, userid, buynow, startprice, enddate, photoid) VALUES(?,?,?,?,?,?,?,?)");
+        stmt.setString(1,brand);
+        stmt.setString(2,model);
+        stmt.setString(3,descr);
+        stmt.setString(4,userid);
+        stmt.setString(5,buynow);
+        stmt.setString(6,startprice);
+        stmt.setString(7, enddate);
+        stmt.setString(8, photoid);
+        stmt.executeUpdate();
+        
+        String phototype = ".jpg";
+        //*Blob photo = ; /!!Kuva on kannassa Blob muodossa, ilmeisesti näin sen pitää olla? Tähän on määriteltävä Blob arvo!!
+        String photosize = "100x100";
+        String photoname = "hienojopo";
+        
+        
+        //Lisää kuvan viimeksi lisätyn itemidn perusteella
+        PreparedStatement stmt2 = con.prepareStatement("INSERT INTO photos(itemid, phototype, photosize, photoname) VALUES (LAST_INSERT_ID(),?,?,?)");
+        stmt2.setString(1, phototype);
+        //*stmt2.setBlob(2, ); //!!BLOB ARVO MÄÄRITELTÄVÄ JA LISÄTTÄVÄ, MYÖS QUERYYN YKSI ? MERKKI LISÄÄ!!
+        stmt2.setString(2, photosize);
+        stmt2.setString(3, photoname);
+        stmt2.executeUpdate();
+
+        con.close();
+    }catch(Exception e){ System.out.println(e);}
+}
+
+//Poista esine
+static void deleteItem(){
+    try{
+        Class.forName("com.mysql.jdbc.Driver");  
+        Connection con=DriverManager.getConnection(  
+        "jdbc:mysql://localhost:3306/auctions?zeroDateTimeBehavior=convertToNull", "root", "root");
+        
+        int itemid = 19; //Queryssä käytettävä arvo
+        PreparedStatement stmt = con.prepareStatement("DELETE FROM users WHERE userid=?");
+        stmt.setInt(1,itemid);
+        stmt.executeUpdate();
+        con.close();
+    }catch(Exception e){ System.out.println(e);}
+}
+
+//Lisää tarjous
+static void addBid(){
+    try{Class.forName("com.mysql.jdbc.Driver");  
+        Connection con=DriverManager.getConnection(  
+        "jdbc:mysql://localhost:3306/auctions?zeroDateTimeBehavior=convertToNull", "root", "root");
+       
+        String itemid = "";
+        String userid = "";
+        String bid = "";
+        PreparedStatement stmt = con.prepareStatement("INSERT INTO bids(itemid, userid, bid) VALUES (?,?,?)");
+        stmt.setString(1, itemid);
+        stmt.setString(2, userid);
+        stmt.setString(3, bid);
+        stmt.executeUpdate();
+        con.close();
+    }catch(Exception e){ System.out.println(e);}
+}
+
+//Hakee kaikki esineet
+static void getItems(){
+    try{Class.forName("com.mysql.jdbc.Driver");  
+        Connection con=DriverManager.getConnection(  
+        "jdbc:mysql://localhost:3306/auctions?zeroDateTimeBehavior=convertToNull", "root", "root");
+        
+        Statement stmt = con.createStatement();
+        ResultSet rs = stmt.executeQuery("SELECT * FROM items"); //Käytetään ResultSettiä ja määritellään query
+        while(rs.next()){ //Tarvittava looppi, jotta kaikki data saadaan haettua
+            //Määrittää muuttujan nimen jokaiselle sarakkeelle
+            int itemid = rs.getInt("itemid");
+            String brand = rs.getString("brand");
+            String model = rs.getString("model");
+            int userid = rs.getInt("userid");
+            String buynow = rs.getString("buynow");
+            String startprice = rs.getString("startprice");
+            String enddate = rs.getString("enddate");
+            String active = rs.getString("active");
+            String photoid = rs.getString("photoid");
+            rs.getMetaData(); //Hakee metedatan = sarakkeiden nimet, tätä ei kuitenkaan käytetä missään toistaiseksi.
+            //Testiä varten tehty printtaus
+            System.out.println(itemid + "\t" + brand +
+                               "\t" + model + "\t" + userid +
+                               "\t" + startprice);
+        }
+        con.close();
+    }catch(Exception e){ System.out.println(e);}
+}
+
+//Hakee yksittäisen käyttäjän tiedot
+static void getUser(){
+    try{Class.forName("com.mysql.jdbc.Driver");  
+        Connection con=DriverManager.getConnection(  
+        "jdbc:mysql://localhost:3306/auctions?zeroDateTimeBehavior=convertToNull", "root", "root");
+        
+        PreparedStatement stmt=null;
+        ResultSet rs=null;
+        int userid = 1; //Halutun käyttäjän määrittely
+        stmt=con.prepareStatement("SELECT * FROM users WHERE userid=?");
+        stmt.setInt(1,userid);
+        rs=stmt.executeQuery();
+        while(rs.next())
+        {
+            userid = rs.getInt("userid");
+            String firstname = rs.getString("firstname");
+            String lastname = rs.getString("lastname");
+            String email = rs.getString("email");
+            String phone = rs.getString("phone");
+            String password = rs.getString("password");
+            String username = rs.getString("username");
+            rs.getMetaData();
+            //Testi tulostus
+            System.out.println(userid + "\t" + firstname +
+                               "\t" + lastname + "\t" + email +
+                               "\t" + username);
+            
+        }
+        con.close();
+    }catch(Exception e){ System.out.println(e);}
+}
+
+//Hakee halutun esineen tiedot, kuvan ja esineelle tehdyt tarjoukset
+static void getItemInfo(){
+    try{Class.forName("com.mysql.jdbc.Driver");  
+        Connection con=DriverManager.getConnection(  
+        "jdbc:mysql://localhost:3306/auctions?zeroDateTimeBehavior=convertToNull", "root", "root");
+        
+        ResultSet rs=null;
+        int itemid = 7; //Halutun esineen määrittely
+        PreparedStatement stmt = con.prepareStatement("SELECT * FROM items AS i LEFT JOIN photos AS p ON i.itemid=p.itemid LEFT JOIN bids AS b ON i.itemid=b.itemid WHERE i.itemid=?");
+        stmt.setInt(1,itemid);
+        rs = stmt.executeQuery();
+        while(rs.next()){
+            itemid = rs.getInt("itemid");
+            String brand = rs.getString("brand");
+            String model = rs.getString("model");
+            int userid = rs.getInt("userid");
+            String buynow = rs.getString("buynow");
+            String startprice = rs.getString("startprice");
+            String enddate = rs.getString("enddate");
+            String active = rs.getString("active");
+            int photoid = rs.getInt("photoid");
+            String phototype = rs.getString("phototype");
+            //*Blob photo = rs2.getString(photo); //!!KUVAN MÄÄRITTELY!!
+            String photosize = rs.getString("photosize");
+            String photoname = rs.getString("photoname");
+            int bidid = rs.getInt("bidid");
+            int userid2 = rs.getInt("b.userid");
+            String bid = rs.getString("bid");
+            
+            rs.getMetaData();
+            //Testi tulostus
+            System.out.println(itemid + "\t" + brand +
+                               "\t" + model + "\t" + userid +
+                               "\t" + photoname + "\t" + userid2 + "\t" + bid);
+        }
+        con.close();
+    }catch(Exception e){ System.out.println(e);}
+}
+
+//Hakee tietyn käyttäjän kaikki esineet ja niiden kuvat
+static void getUsersItems(){
+    try{Class.forName("com.mysql.jdbc.Driver");  
+        Connection con=DriverManager.getConnection(  
+        "jdbc:mysql://localhost:3306/auctions?zeroDateTimeBehavior=convertToNull", "root", "root");
+        
+        ResultSet rs=null;
+        int userid = 4; //Käyttäjän määrittely
+        PreparedStatement stmt = con.prepareStatement("SELECT * FROM items AS i LEFT JOIN photos AS p ON i.itemid=p.itemid WHERE i.userid=?");
+        stmt.setInt(1,userid);
+        rs = stmt.executeQuery();
+        while(rs.next()){
+            int itemid = rs.getInt("itemid");
+            String brand = rs.getString("brand");
+            String model = rs.getString("model");
+            userid = rs.getInt("userid");
+            String buynow = rs.getString("buynow");
+            String startprice = rs.getString("startprice");
+            String enddate = rs.getString("enddate");
+            String active = rs.getString("active");
+            int photoid = rs.getInt("photoid");
+            String phototype = rs.getString("phototype");
+            //*Blob photo = rs2.getString(photo); //!!KUVAN MÄÄRITTELY!!
+            String photosize = rs.getString("photosize");
+            String photoname = rs.getString("photoname");
+            
+            rs.getMetaData();
+            System.out.println(itemid + "\t" + brand +
+                               "\t" + model + "\t" + userid +
+                               "\t" + photoname);
+        }
+        con.close();
+    }catch(Exception e){ System.out.println(e);}
+}
+
+//Tietyn käyttäjän tekemät tarjoukset
+static void getUsersBids(){
+    try{Class.forName("com.mysql.jdbc.Driver");  
+        Connection con=DriverManager.getConnection(  
+        "jdbc:mysql://localhost:3306/auctions?zeroDateTimeBehavior=convertToNull", "root", "root");
+        
+        ResultSet rs=null;
+        int userid = 3; //Käyttäjän määrittely
+        PreparedStatement stmt = con.prepareStatement("SELECT * FROM bids WHERE userid=?");
+        stmt.setInt(1,userid);
+        rs = stmt.executeQuery();
+        while(rs.next()){
+            userid = rs.getInt("userid");
+            int bidid = rs.getInt("bidid");
+            int itemid = rs.getInt("itemid");
+            String bid = rs.getString("bid");
+            
+            rs.getMetaData();
+            //Testi tulostus
+            System.out.println(itemid + "\t" + bidid +
+                               "\t" + bid + "\t" + userid);
+        }
+        con.close();
+    }catch(Exception e){ System.out.println(e);}
+}
+
+
+}
